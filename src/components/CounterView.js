f25207982595bb65eb9db96a1ddcbc9f4a61984f
Yoshiyuki Kondo <cond@st.rim.or.jp>
2017-03-19 15:52:06 +0900

    Created UI in 'components/CounterView.js'

---
+++ src/components/CounterView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CounterView = () => {
+    return (
+	<div>
+	    <div>
+	    {100}
+	    </div>
+	    <button>+1</button>
+	    <button>-1</button>
+	    <button>Reset</button>
+	</div>
+    )
+}
+
+export default CounterView
